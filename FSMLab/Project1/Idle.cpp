5e870a19399633ec573f5a67dc72afcfcf489815
Daragh Behan <C00195564@itcarlow.ie>
2017-10-12 09:21:02 +0100

    FSMLab updated

--- FSMLab/Project1/Idle.cpp
+++ FSMLab/Project1/Idle.cpp
@@ -1,15 +1,16 @@
 #include "Idle.h"
-
+#include "Jumping.h"
+#include "Climbing.h"
 void Idle::jumping(Animation* a)
 {
 	std::cout << "jumping" << std::endl;
-	a->setcurrent(new Idle());
+	a->setcurrent(new Jumping());
 	delete this;
 }
 
 void Idle::climbing(Animation* a)
 {
 	std::cout << "climbing" << std::endl;
-	a->setcurrent(new Idle());
+	a->setcurrent(new Climbing());
 	delete this;
 }
